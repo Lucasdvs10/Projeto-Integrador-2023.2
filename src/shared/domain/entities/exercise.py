564491cc7eff632b74d4218aa8673b02b75ec7e8
Lucas Duez <lucas.verzaro@gmail.com>
2023-10-20 14:02:29 +0000

    continuando exercise entity

--- src/shared/domain/entities/exercise.py
+++ src/shared/domain/entities/exercise.py
@@ -9,7 +9,7 @@ class Exercise:
     _title: str
     _enunciado: str
     _creation_date: datetime
-    _expiration_date: dataclasses
+    _expiration_date: datetime
     _correct_answer: str
 
     def __init__(self, exercise_id: str, discipline_id, title, enunciado, creation_date, exipiration_date, correct_answer):
@@ -20,3 +20,67 @@ class Exercise:
         self._creation_date = creation_date
         self._expiration_date = exipiration_date
         self._correct_answer = correct_answer
+
+
+    @property
+    def exercise_id(self):
+        return self._exercise_id
+
+    @exercise_id.setter
+    def exercise_id(self, value):
+        if value is None:
+            return
+        self.exercise_id = value
+
+    @property
+    def discipline_id(self):
+        return self._exercise_id
+
+    @discipline_id.setter
+    def discipline_id(self, value):
+        if value is None:
+            return
+        self.discipline_id = value
+
+    @property
+    def title(self):
+        return self._exercise_id
+
+    @title.setter
+    def title(self, value):
+        if value is None:
+            return
+        self.discipline_id = value
+
+
+    @property
+    def enunciado(self):
+        return self._exercise_id
+
+    @enunciado.setter
+    def enunciado(self, value):
+        if value is None:
+            return
+        self._enunciado = value
+
+
+    @property
+    def creation_date(self):
+        return self._exercise_id
+
+    @creation_date.setter
+    def creation_date(self, value):
+        if value is None:
+            return
+        self._creation_date = value
+
+
+
+
+
+
+
+
+
+
+
