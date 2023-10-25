157377d7dd15b1576690c0da6aa6372006b09bf2
Luigi Trevisan <checkzin123@gmail.com>
2023-10-25 15:33:08 -0300

    entity + TU

---
+++ src/shared/domain/entities/user.py
@@ -0,0 +1,130 @@
+import dataclasses
+import re
+from typing import List
+
+from src.shared.domain.enums.role_enum import ROLE
+from src.shared.helpers.errors.domain_errors import EntityParameterTypeError
+
+@dataclasses.dataclass
+class User:
+    _email: str
+    _name: str
+    _role: ROLE
+    _password: str
+    _disciplines : List[str] # List of discipline ids
+    _exercises_solved : List[str] # List of exercise ids
+    NAME_MIN_LENGTH = 3
+    NAME_MAX_LENGTH = 255
+    PASSWORD_MIN_LENGTH = 8
+    PASSWORD_MAX_LENGTH = 255
+    
+    def __init__(self, email, name, role, password, disciplines, exercises_solved):
+        if not self.validate_email(email):
+            raise EntityParameterTypeError("email")
+        self._email = email
+        
+        if type(name) != str:
+            raise EntityParameterTypeError("name")
+        if not self.NAME_MIN_LENGTH <= len(name) <= self.NAME_MAX_LENGTH:
+            raise ValueError(f"Name must be between {self.NAME_MIN_LENGTH} and {self.NAME_MAX_LENGTH} characters long")
+        self._name = name
+        
+        if type(role) != ROLE:
+            raise EntityParameterTypeError("role")
+        if role not in ROLE:
+            raise ValueError(f"Role must be one of {ROLE}")
+        self._role = role
+
+        if type(password) != str:
+            raise EntityParameterTypeError("password")
+        if not self.PASSWORD_MIN_LENGTH <= len(password) <= self.PASSWORD_MAX_LENGTH:
+            raise ValueError(f"Password must be between {self.PASSWORD_MIN_LENGTH} and {self.PASSWORD_MAX_LENGTH} characters long")
+        self._password = password
+        
+        if type(disciplines) != list:
+            raise EntityParameterTypeError("disciplines")
+        # if not all(Discipline.validate_id(discipline_id) for discipline_id in disciplines):
+        #     raise ValueError("Invalid discipline id")
+        self._disciplines = disciplines
+        
+        if type(exercises_solved) != list:
+            raise EntityParameterTypeError("exercises_solved")
+        # if not all(Exercise.validate_id(exercise_id) for exercise_id in exercises_solved):
+        #     raise ValueError("Invalid exercise id")
+        self._exercises_solved = exercises_solved
+        
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, value):
+        if not self.validate_email(value):
+            raise EntityParameterTypeError("email")
+        self._email = value
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if type(value) != str:
+            raise EntityParameterTypeError("name")
+        if not self.NAME_MIN_LENGTH <= len(value) <= self.NAME_MAX_LENGTH:
+            raise ValueError(f"Name must be between {self.NAME_MIN_LENGTH} and {self.NAME_MAX_LENGTH} characters long")
+        self._name = value
+        
+    @property
+    def role(self):
+        return self._role
+    
+    @role.setter
+    def role(self, value):
+        if type(value) != ROLE:
+            raise EntityParameterTypeError("role")
+        if value not in ROLE:
+            raise ValueError(f"Role must be one of {ROLE}")
+        self._role = value
+        
+    @property
+    def password(self):
+        return self._password
+    
+    @password.setter
+    def password(self, value):
+        if type(value) != str:
+            raise EntityParameterTypeError("password")
+        if not self.PASSWORD_MIN_LENGTH <= len(value) <= self.PASSWORD_MAX_LENGTH:
+            raise ValueError(f"Password must be between {self.PASSWORD_MIN_LENGTH} and {self.PASSWORD_MAX_LENGTH} characters long")
+        self._password = value
+        
+    @property
+    def disciplines(self):
+        return self._disciplines
+    
+    @disciplines.setter
+    def disciplines(self, value):
+        if type(value) != list:
+            raise EntityParameterTypeError("disciplines")
+        # if not all(Discipline.validate_id(discipline_id) for discipline_id in value):
+        #     raise ValueError("Invalid discipline id")
+        self._disciplines = value
+        
+    @property
+    def exercises_solved(self):
+        return self._exercises_solved
+    
+    @exercises_solved.setter
+    def exercises_solved(self, value):
+        if type(value) != list:
+            raise EntityParameterTypeError("exercises_solved")
+        # if not all(Exercise.validate_id(exercise_id) for exercise_id in value):
+        #     raise ValueError("Invalid exercise id")
+        self._exercises_solved = value
+     
+    def validate_email(self, email):
+        if type(email) != str:
+            return False
+        regex = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+        return bool(re.fullmatch(regex, email))
---
+++ tests/shared/domain/entities/test_user.py
@@ -0,0 +1,89 @@
+import pytest
+
+from src.shared.domain.entities.user import User
+from src.shared.domain.enums.role_enum import ROLE
+from src.shared.helpers.errors.domain_errors import EntityParameterTypeError
+
+class Test_User:
+    def test_instantiate_user(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        assert user
+        
+    def test_change_user_email(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        user.email = "outroemail@maua.br"
+
+        assert user.email == "outroemail@maua.br"
+        
+    def test_change_user_email_to_a_invalid_one(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        with(pytest.raises(EntityParameterTypeError)):
+            user.email = "outroemailmaua.br"
+            
+    def test_change_user_name(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        user.name = "Outro nome"
+
+        assert user.name == "Outro nome"
+        
+    def test_change_user_name_to_a_invalid_one(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        with(pytest.raises(EntityParameterTypeError)):
+            user.name = 42
+            
+    def test_change_user_role(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        user.role = ROLE.STUDENT
+        
+        assert user.role == ROLE.STUDENT
+        
+    def test_change_user_role_to_a_invalid_one(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        with(pytest.raises(EntityParameterTypeError)):
+            user.role = "Monitor"
+            
+    def test_change_user_password(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        user.password = "Outra senha"
+        
+        assert user.password == "Outra senha"
+        
+    def test_change_user_password_to_a_invalid_one(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        with(pytest.raises(EntityParameterTypeError)):
+            user.password = 42
+            
+    def test_change_user_disciplines_list(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        user.disciplines = [1,2,3]
+        
+        assert user.disciplines == [1,2,3]
+        
+    def test_change_user_disciplines_list_to_a_invalid_one(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        with(pytest.raises(EntityParameterTypeError)):
+            user.disciplines = "Uma lista em string"
+            
+    def test_change_user_exercises_solved_list(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        user.exercises_solved = [1,2,3]
+        
+        assert user.exercises_solved == [1,2,3]
+        
+    def test_change_user_exercises_solved_list_to_a_invalid_one(self):
+        user = User(email="22.01102-0@maua.br", name="João", role=ROLE.MONITOR, password="Teste123", disciplines=[], exercises_solved=[])
+        
+        with(pytest.raises(EntityParameterTypeError)):
+            user.exercises_solved = "Uma lista em string"
