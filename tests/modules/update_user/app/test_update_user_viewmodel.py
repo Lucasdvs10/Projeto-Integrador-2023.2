2ac5e2f34eae5a5a9ef898c700507610fa7fcd63
Luigi Trevisan <checkzin123@gmail.com>
2023-11-10 18:03:59 -0300

    update user viewmodel + TU

---
+++ tests/modules/update_user/app/test_update_user_viewmodel.py
@@ -0,0 +1,16 @@
+from src.modules.update_user.app.update_user_usecase import UpdateUserUsecase
+from src.modules.update_user.app.update_user_viewmodel import UpdateUserViewmodel
+from src.shared.domain.enums.role_enum import ROLE
+from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
+
+
+class Test_UpdateUserViewmodel:
+    def test_update_user_viewmodel(self):
+        repo = UserRepositoryMock()
+        usecase = UpdateUserUsecase(repo)
+        email = repo.get_all_users()[0].email
+        updated_user = usecase(email=email, new_name="Luizinho")
+        viewmodel = UpdateUserViewmodel(updated_user).to_dict()
+        expected = {'user': {'email': '22.01102-0@maua.br', 'name': 'Luizinho', 'role': 'MONITOR', 'exercises_solved': []}, 'message': 'User updated successfully'}
+        
+        assert viewmodel == expected
