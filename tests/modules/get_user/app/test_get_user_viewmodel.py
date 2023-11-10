bca9eba56f4d4caab66e712a329da353e1cda781
Luigi Trevisan <checkzin123@gmail.com>
2023-11-10 17:13:57 -0300

    get user viewmodel + TU

---
+++ tests/modules/get_user/app/test_get_user_viewmodel.py
@@ -0,0 +1,16 @@
+from src.modules.get_user.app.get_user_usecase import GetUserUsecase
+from src.modules.get_user.app.get_user_viewmodel import GetUserViewmodel
+from src.shared.domain.enums.role_enum import ROLE
+from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
+
+
+class Test_GetUserViewmodel:
+    def test_Get_user_viewmodel(self):
+        repo = UserRepositoryMock()
+        usecase = GetUserUsecase(repo)
+        email = repo.get_all_users()[0].email
+        new_user = usecase(email=email)
+        viewmodel = GetUserViewmodel(new_user).to_dict()
+        expected = {'user': {'email': '22.01102-0@maua.br', 'name': 'Luigi Trevisan', 'role': 'MONITOR', 'exercises_solved': []}, 'message': 'User retrieved successfully'}
+        
+        assert viewmodel == expected
