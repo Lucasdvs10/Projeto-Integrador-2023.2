f93c13988ed70104454085d4029544d6ad1ebcbc
Luigi Trevisan <checkzin123@gmail.com>
2023-11-10 10:47:32 -0300

    controller + TU

---
+++ src/modules/get_ranking/app/get_ranking_controller.py
@@ -0,0 +1,18 @@
+from .get_ranking_usecase import GetRankingUsecase
+from .get_ranking_viewmodel import GetRankingViewmodel
+from src.shared.helpers.external_interfaces.http_codes import OK, InternalServerError
+
+
+class GetRankingController:
+    
+    def __init__(self, usecase: GetRankingUsecase):
+        self.usecase = usecase
+        
+    def __call__(self) -> dict:
+        try:
+            ranking = self.usecase()
+            viewmodel = GetRankingViewmodel(ranking)
+            return OK(viewmodel.to_dict())
+
+        except Exception as err:
+            return InternalServerError(body=err.args[0])
