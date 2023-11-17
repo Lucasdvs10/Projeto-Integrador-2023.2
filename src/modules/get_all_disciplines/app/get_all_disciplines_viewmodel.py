90275272473f8d502694bfbf963d61882202a6e7
Luigi Trevisan <checkzin123@gmail.com>
2023-11-17 10:31:47 -0300

    get all disciplines viewmodel + TU

---
+++ src/modules/get_all_disciplines/app/get_all_disciplines_viewmodel.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from src.shared.domain.entities.discipline import Discipline
+
+
+class DisciplineViewmodel:
+    name: str
+    discipline_id: str
+    year: int
+    students_emails_list: List[str]
+    
+    def __init__(self, discipline):
+        self.name = discipline.name
+        self.discipline_id = discipline.discipline_id
+        self.year = discipline.year
+        self.students_emails_list = discipline.students_emails_list
+        
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "discipline_id": self.discipline_id,
+            "year": self.year,
+            "students_emails_list": self.students_emails_list
+        }
+        
+class GetAllDisciplinesViewmodel:
+    disciplines: List[DisciplineViewmodel]
+    
+    def __init__(self, disciplines: List[Discipline]):
+        self.disciplines = [DisciplineViewmodel(discipline) for discipline in disciplines]
+        
+    def to_dict(self):
+        return {
+            "disciplines": [discipline.to_dict() for discipline in self.disciplines],
+            "message": "Disciplines found successfully"
+        }
