6acdbfccd5c6cb51051c49ccf2c24e41b94c615c
Luigi Trevisan <checkzin123@gmail.com>
2023-11-17 22:58:00 -0300

    delete discipline viewmodel + TU

---
+++ src/modules/delete_discipline/app/delete_discipline_viewmodel.py
@@ -0,0 +1,34 @@
+from typing import List
+from src.shared.domain.entities.discipline import Discipline
+
+class DisciplineViewmodel:
+    name: str
+    discipline_id: str
+    year: int
+    students_emails_list: List[str]
+    
+    def __init__(self, discipline):
+        self.name = discipline.name
+        self.discipline_id = discipline.discipline_id
+        self.year = discipline.year
+        self.students_emails_list = discipline.students_emails_list
+        
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "discipline_id": self.discipline_id,
+            "year": self.year,
+            "students_emails_list": self.students_emails_list
+        }
+        
+class DeleteDisciplineViewmodel:
+    discipline: DisciplineViewmodel
+    
+    def __init__(self, discipline: Discipline):
+        self.discipline = DisciplineViewmodel(discipline)
+        
+    def to_dict(self):
+        return {
+            "discipline": self.discipline.to_dict(),
+            "message": "Discipline deleted successfully"
+        }
