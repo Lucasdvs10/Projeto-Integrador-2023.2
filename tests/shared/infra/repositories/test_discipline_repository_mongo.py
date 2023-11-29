5c37eafcd84560335603f2211d2980cc11378618
Luigi Trevisan <checkzin123@gmail.com>
2023-11-29 15:52:41 -0300

    @pytest.mark.skip(reason="MongoDB is not available")

--- tests/shared/infra/repositories/test_discipline_repository_mongo.py
+++ tests/shared/infra/repositories/test_discipline_repository_mongo.py
@@ -4,6 +4,7 @@ from src.shared.infra.repositories.discipline_repository_mongo import Discipline
 import pytest
 
 class Test_DisciplineRepositoryMongo:
+    @pytest.mark.skip(reason="MongoDB is not available")
     def test_batch_create_disciplines(self):
         repo_mongo = DisciplineRepositoryMongo()
         repo_mock = DisciplineRepositoryMock()
@@ -14,6 +15,7 @@ class Test_DisciplineRepositoryMongo:
         assert resp == disciplines
         assert len(repo_mongo.get_all_disciplines()) == len_before + len(disciplines)
         
+    @pytest.mark.skip(reason="MongoDB is not available")
     def test_create_discipline(self):
         repo_mongo = DisciplineRepositoryMongo()
         len_before = len(repo_mongo.get_all_disciplines())
@@ -22,24 +24,28 @@ class Test_DisciplineRepositoryMongo:
         resp = repo_mongo.create_discipline(discipline)
         assert resp == discipline
         assert len(repo_mongo.get_all_disciplines()) == len_before + 1
-        
+      
+    @pytest.mark.skip(reason="MongoDB is not available")  
     def test_get_discipline_by_id(self):
         repo_mongo = DisciplineRepositoryMongo()
         resp = repo_mongo.get_discipline_by_id("123")
         assert resp == Discipline(name="Teste", discipline_id="123", year=2021, students_emails_list=[])
-        
+    
+    @pytest.mark.skip(reason="MongoDB is not available")    
     def test_update_discipline_by_id(self):
         repo_mongo = DisciplineRepositoryMongo()
         resp = repo_mongo.update_discipline_by_id("123", new_name="Teste2")
         assert resp == Discipline(name="Teste2", discipline_id="123", year=2021, students_emails_list=[])
-        
+    
+    @pytest.mark.skip(reason="MongoDB is not available")    
     def test_delete_discipline(self):
         repo_mongo = DisciplineRepositoryMongo()
         len_before = len(repo_mongo.get_all_disciplines())
         resp = repo_mongo.delete_discipline("123")
         assert resp == Discipline(name="Teste2", discipline_id="123", year=2021, students_emails_list=[])
         assert len(repo_mongo.get_all_disciplines()) == len_before - 1
-        
+       
+    @pytest.mark.skip(reason="MongoDB is not available")   
     def test_get_all_disciplines(self):
         repo_mongo = DisciplineRepositoryMongo()
         resp = repo_mongo.get_all_disciplines()
